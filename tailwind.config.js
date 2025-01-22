f0ea9ac5eb978bac60d5d7f2a7c29c28b0512c1f
hosseinGith <hosseinderee7@gmail.com>
2025-01-22 19:49:51 +0330

    product status & web site styles scripts settings

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      bodyBg: "#f6f6f6",
+      contentBodyColor: "#fff",
+      lowTextColor: "#3d3d3d",
+      lowLgTextColor: "#888888",
+      highTextColor: "#000",
+      mainBg: "#f01436",
+      mainBgLowOpacity: "#faecee",
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
